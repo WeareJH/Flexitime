28ce86f1b40aaec56a5276bd564d66ccd47a43ee
Aydin Hassan <aydin@hotmail.co.uk>
2014-05-28 00:15:31 +0100

    Update testing.config.php

--- test/testing.config.php
+++ test/testing.config.php
@@ -11,7 +11,6 @@ return [
                     'host'     => '127.0.0.1',
                     'port'     => '3306',
                     'user'     => 'root',
-                    'password' => 'test',
                     'dbname'   => 'flex_test'
                 ]
             ],
